dd2bbdb211d592fdf5ad4698b7faa9d3f4d8041a
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2014-12-15 15:27:17 -0500

    changed location of slider

--- page-home.php
+++ page-home.php
@@ -6,14 +6,23 @@ Template Name: Home Page Template
 
 <?php get_header(); ?>
 
-<div class="homebanner">
+<div class="slider">
   <div class="container">
-    <div class="row">
-      <div class="col-md-8 col-md-offset-2">
-        <h1>BREW for Wordpress</h1>
-        <p>A free and open-source starter theme based on Bones and Bootstrap 3</p>
-        <p><a href="http://www.github.com/slightlyoffbeat/brew" target="_blank" class="btn btn-default btn-lg">Github</a>
-          <a href="http://danvswild.com" target="_blank" class="btn btn-lg btn-primary">Author</a></p>
+    
+      <div class="row text-center">
+        <div class="sl-test">
+     <?php $feat_posts = get_posts('category=66'); ?>
+    <div class="liquid-slider col-md-8" id ="slider2" style="width=100%;">
+       <?php foreach($feat_posts as $post) { ?>
+        <div>
+        <h2 class="title"><?php echo $post->post_title; ?></h2>
+        <p><?php echo $post->post_excerpt; ?></p>
+        <?php echo get_the_post_thumbnail( $post->ID, 'thumbnail') ?>
+        </div>
+      
+      
+       <?php } ?>
+       </div>
       </div>
     </div><!-- end .row-->
   </div> <!-- end .container-->
@@ -22,19 +31,7 @@ Template Name: Home Page Template
 <div class="container">
   <div class="row text-center">
    <!--  <div class="row text-center"> -->
-    <div class="sl-test">
-     <?php $feat_posts = get_posts('category=66'); ?>
-    <div class="liquid-slider col-md-8" id ="slider2" style="width=100%;">
-       <?php foreach($feat_posts as $post) { ?>
-        <div>
-        <h2 class="title"><?php echo $post->post_title; ?></h2>
-        <?php echo get_the_post_thumbnail( $post->ID, 'medium') ?>
-        </div>
-      
-      
-       <?php } ?>
-       </div>
-       </div>
+    
        <!-- </div> -->
        <div class="row text-center">
     <!--Section 1-->

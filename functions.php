040604672f73618febdf5b058325a3aaaa7d35f1
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-01-21 09:40:53 -0500

    deregistered parent theme styles and imported into childtheme stylesheet. Added slider docs to child theme.

--- functions.php
+++ functions.php
@@ -1,19 +1,41 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
-function enqueue_parent_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+// add_action( 'wp_enqueue_scripts', 'remove_default_stylesheet', 25 );
+
+// function remove_default_stylesheet() {
+    
+//     wp_dequeue_style( 'bones-stylesheet' );
+//     wp_deregister_style( 'bones-stylesheet' );
+
+// }
+add_action( 'wp_print_styles', 'my_deregister_styles', 100 );
+
+function my_deregister_styles() {
+	wp_deregister_style( 'bones-stylesheet' );
 }
-
-
 function ls_scripts() {
-		
+		// wp_enqueue_style('brew-child-css', get_stylesheet_directory_uri() . '/library/css/style.css');
 		wp_enqueue_style('ls-css', get_stylesheet_directory_uri() . '/library/css/liquid-slider.css');
+		wp_enqueue_style('brew-child-css', get_stylesheet_directory_uri() . '/library/css/style.css');
 		wp_enqueue_script( 'jquery-easing', get_stylesheet_directory_uri() . 
 			'/library/js/jquery.easing.1.3.js', array('jquery'));
-		wp_enqueue_script( 'jquery-touchSwipe', get_stylesheet_directory_uri() . '/library/js/jquery.touchSwipe.min.js', array('jquery-easing'));
+		wp_enqueue_script( 'jquery-touchSwipe', get_theme_root() . 'BrewChildTheme/library/js/jquery.touchSwipe.min.js', array('jquery-easing'));
 		wp_enqueue_script( 'jquery-ls', get_stylesheet_directory_uri() . '/library/js/jquery.liquid-slider.min.js', array('jquery-touchSwipe'));
+		
+
 
 }
 add_action('wp_enqueue_scripts', 'ls_scripts');
+
+// function bones_scripts_and_styles_child() {
+//   // global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
+  
+    
+//     // register main stylesheet
+//     wp_enqueue_style( 'bones-child-stylesheet', get_stylesheet_directory_uri() . '/library/css/style.css', array(), '', 'all' );
+
+// }
+// add_action('wp_enqueue_style', 'bones_scripts_and_styles_child' );
+
+
 ?>
--- page-home.php
+++ page-home.php
@@ -7,24 +7,26 @@ Template Name: Home Page Template
 <?php get_header(); ?>
 
 <div class="slider">
-  <div  class="container" >
+  <div  class="container">
     
     <div class="sl-test">
-     <?php $feat_posts = get_posts('category=2'); ?>
+     <?php $feat_posts = get_posts('category=174'); ?>
     <div class="liquid-slider" id ="slider2">
        <?php foreach($feat_posts as $post) { ?>
-        <div>
+        <div id="slider-content">
         <h2 class="title"><?php echo $post->post_title; ?></h2>
+         <?php echo get_the_post_thumbnail( $post->ID, 'thumbnail') ?>
+
       <?php 
   
       $trimtitle = get_post_field('post_content', $id);
   
-      $shorttitle = wp_trim_words( $trimtitle, $num_words = 150, $more = '… ' );
+      $shorttitle = wp_trim_words( $trimtitle, $num_words = 50, $more = '… ' );
   
         echo '<p class="info-title">' .  $shorttitle . '</p>';
        ?>
-       <a href="<?php echo get_permalink(); ?>"> Read More...</a>
-        <?php echo get_the_post_thumbnail( $post->ID, 'thumbnail') ?>
+       <a href="<?php echo get_permalink(); ?>" class="btn btn-default"> Read More...</a>
+       
         </div>
       
       
@@ -35,33 +37,57 @@ Template Name: Home Page Template
 </div> <!-- end .slider-->
 
 <div class="container">
-  <div class="row text-center">
+  <div class="row text-center" id="content">
    <!--  <div class="row text-center"> -->
     
        <!-- </div> -->
-       <div class="row text-center">
+       <div class="row">
+    <!--Section 1-->
+    <div class="col-sm-4 about">
+      <i class="fa fa-briefcase fa-4x light-gray"></i>
+      <h3>Job Announcements</h3>
+      <p>Enjoy Wordpress with all of the benefits of Bootstrap.  Quickly build themes while utilizing one of the most powerful frameworks available on the web.</p>
+    </div>
+    
+    <!--Section 2-->
+    <div class="col-sm-4 about">
+      <i class="fa fa-university fa-4x light-gray"></i>
+      <h3>CFPs & Conferences</h3>
+      <p>BREW pulls together some of the best open source projects in to one awesome starter theme.  Feel free to fork, edit and contribute.</p>
+    </div>
+    
+    <!--Section 3-->
+    <div class="col-sm-4 about">
+      <i class="fa fa-info fa-4x light-gray"></i>
+      <h3>Resources</h3>
+      <p>Font Awesome 4 support is already baked in.  Quickly and easily place icons in menus, buttons, headers, lists and more.</p>
+    </div>
+    
+  </div> <!--end row-->
+
+     <div class="row">
     <!--Section 1-->
     <div class="col-sm-4 about">
       <i class="fa fa-laptop fa-4x light-gray"></i>
-      <h3>Built with Bootstrap</h3>
+      <h3>Announcements</h3>
       <p>Enjoy Wordpress with all of the benefits of Bootstrap.  Quickly build themes while utilizing one of the most powerful frameworks available on the web.</p>
     </div>
     
     <!--Section 2-->
     <div class="col-sm-4 about">
       <i class="fa fa-github-square fa-4x light-gray"></i>
-      <h3>Free and Open-Source</h3>
+      <h3>Funding & Opportunities</h3>
       <p>BREW pulls together some of the best open source projects in to one awesome starter theme.  Feel free to fork, edit and contribute.</p>
     </div>
     
     <!--Section 3-->
     <div class="col-sm-4 about">
       <i class="fa fa-flag fa-4x light-gray"></i>
-      <h3>Over 350 Icons</h3>
+      <h3>Reports</h3>
       <p>Font Awesome 4 support is already baked in.  Quickly and easily place icons in menus, buttons, headers, lists and more.</p>
     </div>
     
-  </div>
+  </div> <!--end row-->
 </div>
 </div>
 <?php get_footer(); ?>

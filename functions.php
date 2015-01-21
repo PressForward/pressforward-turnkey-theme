6f5baf7427d4ae34ab3ec6999c452a3cbf33df46
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-01-21 11:38:34 -0500

    fixed icon in content area1.

--- functions.php
+++ functions.php
@@ -40,7 +40,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content1',
     'name' => __( 'Content Widget 1', 'bonestheme' ),
     'description' => __( 'The first content area widget.', 'bonestheme' ),
-    'before_widget' => '<i class="fa fa-bullhorn fa-4x light-gray"></i>',
+    'before_widget' => '<i class="fa fa-briefcase fa-4x light-gray"></i>',
     'before_title' => '<h3>',
     'after_title' => '</h3>',
   ));

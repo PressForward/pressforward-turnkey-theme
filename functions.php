b90297948da55016ecc17cfb3bb2f4b7583d2e5e
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-01-21 10:12:43 -0500

    fixed fontawesome icon issue

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ function ls_scripts() {
 		wp_enqueue_style('brew-child-css', get_stylesheet_directory_uri() . '/library/css/style.css');
 		wp_enqueue_script( 'jquery-easing', get_stylesheet_directory_uri() . 
 			'/library/js/jquery.easing.1.3.js', array('jquery'));
-		wp_enqueue_script( 'jquery-touchSwipe', get_theme_root() . 'BrewChildTheme/library/js/jquery.touchSwipe.min.js', array('jquery-easing'));
+		wp_enqueue_script( 'jquery-touchSwipe', get_stylesheet_directory_uri() . '/library/js/jquery.touchSwipe.min.js', array('jquery-easing'));
 		wp_enqueue_script( 'jquery-ls', get_stylesheet_directory_uri() . '/library/js/jquery.liquid-slider.min.js', array('jquery-touchSwipe'));
 		
 

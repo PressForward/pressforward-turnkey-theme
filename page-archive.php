889925e2ea66aaf32b957c9a4e9d9b73fa3e3d8c
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-01-28 09:52:10 -0500

    wrote php loop to generate category archives within bootstrap accordion & tabs.

--- page-archive.php
+++ page-archive.php
@@ -7,72 +7,52 @@ Template Name: Archive Test
 <?php get_header(); ?>
 	<div class="container">
 	<div class="row">
-	<div class="col-md-12">			
-		<header class="page-head article-header">
-                
-                <div class=""><h1 class="page-title entry-title" itemprop="headline"><?php the_title(); ?></h1></div>
-              
-              </header> <!-- end article header -->
-			<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-			  <div class="panel panel-default">
-			    <div class="panel-heading" role="tab" id="headingOne">
-			      <h4 class="panel-title">
-			        <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-			         	2015
-			        </a>
-			      </h4>
-			    </div>
-			    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-			      <div class="panel-body">
-			        <ul>
-			         <?php $feat_posts = get_posts('category=featured&posts_per_page=5000'); ?>
-			         <?php foreach($feat_posts as $post) { ?>
-						<li><?php echo $post->post_title; ?></li>
-					<?php } ?>
-					</ul>
-			      </div>
-			    </div>
-			  </div>
-			  <div class="panel panel-default">
-			    <div class="panel-heading" role="tab" id="headingTwo">
-			      <h4 class="panel-title">
-			        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-			          2014
-			        </a>
-			      </h4>
-			    </div>
-			    <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-			      <div class="panel-body">
-			         <ul>
-			         <?php $feat_posts = get_posts('category=featured&posts_per_page=5000'); ?>
-			         <?php foreach($feat_posts as $post) { ?>
-						<li><?php echo $post->post_title; ?></li>
-					<?php } ?>
-					</ul>
-			      </div>
-			    </div>
-			  </div>
-			  <div class="panel panel-default">
-			    <div class="panel-heading" role="tab" id="headingThree">
-			      <h4 class="panel-title">
-			        <a class="collapsed" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-			          2013
-			        </a>
-			      </h4>
-			    </div>
-			    <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
-			      <div class="panel-body">
-			         <ul>
-			         <?php $feat_posts = get_posts('category=featured&posts_per_page=5000'); ?>
-			         <?php foreach($feat_posts as $post) { ?>
-						<li><?php echo $post->post_title; ?></li>
-					<?php } ?>
-					</ul>
-			      </div>
-			    </div>
-			  </div>
-			</div>
+	<div class="col-md-12">	
+	
+
+
+	<div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+<?php
+
+query_posts(array('nopaging' => 1, 'category__and' => array(66) /* we want all posts, so disable paging. Order by date is default */));
+$prev_year = null;
+if ( have_posts() ) {
+   while ( have_posts() ) {
+      the_post();
+      $this_year = get_the_date('Y');
+      if ($prev_year != $this_year) {
+          // Year boundary
+          if (!is_null($prev_year)) {
+             // A list is already open, close it first
+             echo '</div>
+    </div>
+  </div>';
+          }
+          echo '<div class="panel panel-default">
+    <div class="panel-heading" role="tab" id="heading' . $this_year . '">
+      <h4 class="panel-title">
+        <a data-toggle="collapse" data-parent="#accordion" href="#collapse' . $this_year . '" aria-expanded="true" aria-controls="collapse' . $this_year . '">' . $this_year . '</a>
+      </h4>
+    </div>';
+          echo '<div id="collapse' . $this_year . '" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading' . $this_year . '">
+      <div class="panel-body">';
+      }
+      echo '<li>';
+      echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a>';
+      echo '</li>'; 
+      $prev_year = $this_year;
+   }
+   echo '</div>
+    </div>
+  </div>';
+}
+
+?>
+		</div>
 		</div>
 		</div>
 		</div>
 <?php get_footer(); ?>
+
+
+<!-- http://wordpress.stackexchange.com/questions/46136/archive-by-year -->

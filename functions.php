452882131bab08814a9af9433898f91a380c5565
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-02-04 13:27:29 -0500

    removed the bones excerpt function.

--- functions.php
+++ functions.php
@@ -27,6 +27,10 @@ function ls_scripts() {
 }
 add_action('wp_enqueue_scripts', 'ls_scripts');
 
+  function remove_read_more_setup() {
+    remove_filter('remove_bones_excerpt', 'bones_excerpt_more');
+  }
+  add_action('remove_read_more', 'remove_read_more_setup');
 // function bones_scripts_and_styles_child() {
 //   // global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
   
@@ -40,7 +44,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content1',
     'name' => __( 'Content Widget 1', 'bonestheme' ),
     'description' => __( 'The first content area widget.', 'bonestheme' ),
-    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-briefcase fa-4x light-gray"></i>',
+    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-briefcase fa-3x light-gray"></i>',
     'after_widget' => '</div>',
     'before_title' => '<h1>',
     'after_title' => '</h1>',
@@ -50,7 +54,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content2',
     'name' => __( 'Content Widget 2', 'bonestheme' ),
     'description' => __( 'The second content area widget.', 'bonestheme' ),
-    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-bullhorn fa-4x light-gray"></i>',
+    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-bullhorn fa-3x light-gray"></i>',
      'after_widget' => '</div>',
     'before_title' => '<h1>',
     'after_title' => '</h1>',
@@ -60,7 +64,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content3',
     'name' => __( 'Content Widget 3', 'bonestheme' ),
     'description' => __( 'The third content area widget.', 'bonestheme' ),
-    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-info fa-4x light-gray"></i>',
+    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-info fa-3x light-gray"></i>',
      'after_widget' => '</div>',
     'before_title' => '<h1>',
     'after_title' => '</h1>',
@@ -70,7 +74,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content4',
     'name' => __( 'Content Widget 4', 'bonestheme' ),
     'description' => __( 'The fourth content area widget.', 'bonestheme' ),
-    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-laptop fa-4x light-gray"></i>',
+    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-laptop fa-3x light-gray"></i>',
      'after_widget' => '</div>',
      'before_title' => '<h1>',
     'after_title' => '</h1>',
@@ -80,7 +84,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content5',
     'name' => __( 'Content Widget 5', 'bonestheme' ),
     'description' => __( 'The fifth content area widget.', 'bonestheme' ),
-    'before_widget' => '<div class="homeinnercontent"><a href="http://localhost:8888/2014/category/job/"><i class="fa fa-money fa-4x light-gray"></i></a>',
+    'before_widget' => '<div class="homeinnercontent"><a href="http://localhost:8888/2014/category/job/"><i class="fa fa-money fa-3x light-gray"></i></a>',
     'after_widget' => '</div>',
     'before_title' => '<h1>',
     'after_title' => '</h1>',
@@ -90,7 +94,7 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'id' => 'content6',
     'name' => __( 'Content Widget 6', 'bonestheme' ),
     'description' => __( 'The sixth content area widget.', 'bonestheme' ),
-    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-flag fa-4x light-gray"></i>',
+    'before_widget' => '<div class="homeinnercontent"><i class="fa fa-flag fa-3x light-gray"></i>',
      'after_widget' => '</div>',
     'before_title' => '<h1>',
     'after_title' => '</h1>',
@@ -106,5 +110,54 @@ add_action('wp_enqueue_scripts', 'ls_scripts');
     'after_title' => '</h1>',
   ));
 
+       register_sidebar(array(
+    'id' => 'participate1',
+    'name' => __( 'Partcipate Block 1 Widget', 'bonestheme' ),
+    'description' => __( 'The first participate block widget.', 'bonestheme' ),
+    'before_widget' => '<div id="%1$s" class="widget participatewidget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<i class="fa fa-question-circle fa-3x"></i><h1 class="widgettitle">',
+    'after_title' => '</h1>',
+  ));
+
+         register_sidebar(array(
+    'id' => 'participate2',
+    'name' => __( 'Partcipate Block 2 Widget', 'bonestheme' ),
+    'description' => __( 'The second participate block widget.', 'bonestheme' ),
+    'before_widget' => '<div id="%1$s" class="widget participatewidget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<i class="fa fa-rss fa-3x"></i><h1 class="widgettitle">',
+    'after_title' => '</h1>',
+  ));
+
+           register_sidebar(array(
+    'id' => 'participate3',
+    'name' => __( 'Partcipate Block 3 Widget', 'bonestheme' ),
+    'description' => __( 'The third participate block widget.', 'bonestheme' ),
+    'before_widget' => '<div id="%1$s" class="widget participatewidget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<i class="fa fa-user fa-3x"></i><h1 class="widgettitle">',
+    'after_title' => '</h1>',
+  ));
+
+             register_sidebar(array(
+    'id' => 'participate4',
+    'name' => __( 'Partcipate Block 4 Widget', 'bonestheme' ),
+    'description' => __( 'The fourth participate block widget.', 'bonestheme' ),
+    'before_widget' => '<div id="%1$s" class="widget participatewidget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<i class="fa fa-calendar fa-3x"></i><h1 class="widgettitle">',
+    'after_title' => '</h1>',
+  ));
+
+             register_sidebar(array(
+    'id' => 'blog',
+    'name' => __( 'Blog Widget', 'bonestheme' ),
+    'description' => __( 'The blog widget.', 'bonestheme' ),
+    'before_widget' => '<div id="%1$s" class="widget blogwidget %2$s">',
+    'after_widget' => '</div>',
+    'before_title' => '<h1 class="widgettitle">',
+    'after_title' => '</h1>',
+  ));
 
 ?>

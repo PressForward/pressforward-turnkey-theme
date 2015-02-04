ac7ee2a099d326d7313126bf1929928644958ea5
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-02-04 14:21:02 -0500

    styled footer. fixed rollover link styles.

---
+++ footer.php
@@ -0,0 +1,57 @@
+    <footer id="footer" class="clearfix">
+      <div id="footer-widgets">
+
+        <div class="container">
+
+        <div id="footer-wrapper">
+
+          <div class="row">
+            <div class="col-sm-6 col-md-4">
+              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-1') ) : ?>
+              <?php endif; ?>
+            </div> <!-- end widget1 -->
+
+            <div class="col-sm-6 col-md-4">
+              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-2') ) : ?>
+              <?php endif; ?>
+            </div> <!-- end widget1 -->
+
+            <div class="col-sm-6 col-md-4">
+              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-3') ) : ?>
+              <?php endif; ?>
+            </div> <!-- end widget1 -->
+
+            <!-- <div class="col-sm-6 col-md-3">
+              <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-4') ) : ?>
+              <?php endif; ?>
+            </div>  end widget1 -->
+
+          </div> <!-- end .row -->
+
+        </div> <!-- end #footer-wrapper -->
+
+        </div> <!-- end .container -->
+      </div> <!-- end #footer-widgets -->
+
+      <div id="sub-floor">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-4 copyright">
+              &copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?>
+            </div>
+            <div class="col-md-4 col-md-offset-4 attribution">
+              <p>Based on <a href="http://www.getbootstrap.com">Bootstrap</a>, <a href="http://www.html5bones.com/">Bones</a>, and <a href="https://github.com/slightlyoffbeat/brew">Brew by Dan Wild</a>.</p> 
+            </div>
+          </div> <!-- end .row -->
+        </div>
+      </div>
+
+    </footer> <!-- end footer -->
+
+    <!-- all js scripts are loaded in library/bones.php -->
+    <?php wp_footer(); ?>
+    <!-- Hello? Doctor? Name? Continue? Yesterday? Tomorrow?  -->
+
+  </body>
+
+</html> <!-- end page. what a ride! -->

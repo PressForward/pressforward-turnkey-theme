384a919476d85296a86684a1731b4615540f9bb7
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-02-10 11:00:01 -0500

    Added new function that removes all the actions from the bones_ahoy function in the parent theme. It then re-adds all of the actions/filters except for the read_more action so that it can be redefined in the child theme.

--- functions.php
+++ functions.php
@@ -27,10 +27,46 @@ function ls_scripts() {
 }
 add_action('wp_enqueue_scripts', 'ls_scripts');
 
-  function remove_read_more_setup() {
-    remove_filter('remove_bones_excerpt', 'bones_excerpt_more');
-  }
-  add_action('remove_read_more', 'remove_read_more_setup');
+//This removes all the actions from the bones_ahoy function and then readds all of them EXCEPT for the excerpt_more. Instead this function removes the 'bones_excerpt_more' function.  Child_bones_excerpt_more re-adds the read more ellipses.
+function remove_ahoy_actions() {
+  remove_action('after_setup_theme', 'bones_ahoy', 16);
+    add_action( 'init', 'bones_head_cleanup' );
+    // remove WP version from RSS
+    add_filter( 'the_generator', 'bones_rss_version' );
+    // remove pesky injected css for recent comments widget
+    add_filter( 'wp_head', 'bones_remove_wp_widget_recent_comments_style', 1 );
+    // clean up comment styles in the head
+    add_action( 'wp_head', 'bones_remove_recent_comments_style', 1 );
+    // clean up gallery output in wp
+    add_filter( 'gallery_style', 'bones_gallery_style' );
+
+    // enqueue base scripts and styles
+    add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+    // ie conditional wrapper
+
+    // launching this stuff after theme setup
+    bones_theme_support();
+
+    // adding sidebars to Wordpress (these are created in functions.php)
+    add_action( 'widgets_init', 'bones_register_sidebars' );
+    // adding the bones search form (created in functions.php)
+    // add_filter( 'get_search_form', 'bones_wpsearch' );
+
+    // cleaning up random code around images
+    add_filter( 'the_content', 'bones_filter_ptags_on_images' );
+    remove_filter('excerpt_more', 'bones_excerpt_more');
+    
+}
+add_action('after_setup_theme', 'remove_ahoy_actions');
+
+
+function child_bones_excerpt_more($more) {
+  global $post;
+  // edit here if you like
+  return '...&nbsp</p>';
+}
+add_filter( 'excerpt_more', 'child_bones_excerpt_more');
+
 // function bones_scripts_and_styles_child() {
 //   // global $wp_styles; // call global $wp_styles variable to add conditional wrapper around ie stylesheet the WordPress way
 add_image_size( 'brew-child-thumbnail', 200, 200, true );  

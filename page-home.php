f09cfc83f694ab705466e2b9dc535d09c46608a1
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-02-10 12:18:20 -0500

    Rearranged blocks and spaced code out.

--- page-home.php
+++ page-home.php
@@ -6,64 +6,69 @@ Template Name: Home Page Template
 
 <?php get_header(); ?>
 
+<!-- BLOCK 1: SLIDER -->
+
 <div class="slider">
  <div  class="container homepage">
-    
     <div class="sl-test">
-     <?php $feat_posts = get_posts('category=66&posts_per_page=4'); ?>
-    <div class="liquid-slider" id ="slider2">
-       
-       <?php foreach($feat_posts as $post) { ?>
-        <div id="slider-content">
-        <?php
-        $trim_title = get_post_field('post_title', $id);
-  
-      $short_title = wp_trim_words( $trim_title, $num_words = 14, $more = '… ' );
-             echo '<div id="slidertitle"><h1 class="title">' . $short_title . '</h1>';
-             echo '<br><h2>By: ' . get_the_author() . '</h2></div>';
-       ?>
-        <!-- <div id="slidertitle"><h1 class="title"><?php echo $post->post_title; ?></h1><br><h2>By: <?php echo get_post_field('item_author') ?></h2></div>  --><!-- close #slidertitle -->
-      
-      <?php 
-  
-      $trimexcerpt = get_post_field('post_content', $id);
-  
-      $shortexcerpt = wp_trim_words( $trimexcerpt, $num_words = 75, $more = '… ' );
-  
-        echo '<div id="slidertext"><p class="info-title">' .  $shortexcerpt . '</p>';
-       ?>
-       <a href="<?php echo get_permalink(); ?>" class="btn btn-default"> Read More...</a></div> <!--close #slidertext -->
-       <div id="sliderimg">
-         <?php echo get_the_post_thumbnail( $post->ID, 'large') ?>
-          </div> <!-- close #sliderimg -->
-        
-       
-       
+          <?php $feat_posts = get_posts('category=66&posts_per_page=4'); ?>
+        <div class="liquid-slider" id ="slider2">
+          <?php foreach($feat_posts as $post) { ?>
+              <div id="slider-content">
+              <?php $trim_title = get_post_field('post_title', $id);
+                    $short_title = wp_trim_words( $trim_title, $num_words = 14, $more = '… ' );
+                    echo '<div id="slidertitle"><h1 class="title">' . $short_title . '</h1>';
+                    echo '<br><h2>By: ' . get_the_author() . '</h2></div>';  ?>
+              <?php $trimexcerpt = get_post_field('post_content', $id);
+                    $shortexcerpt = wp_trim_words( $trimexcerpt, $num_words = 75, $more = '… ' );
+                    echo '<div id="slidertext"><p class="info-title">' .  $shortexcerpt . '</p>';  ?>
+              <a href="<?php echo get_permalink(); ?>" class="btn btn-default"> Read More...</a>
+              </div> <!-- Close #slider-content -->
+               
+              <div id="sliderimg">
+                 <?php echo get_the_post_thumbnail( $post->ID, 'large') ?>
+              </div> <!-- close #sliderimg -->
+
         </div> <!-- close .liquid-slider #slider2 -->
-      
-      
-       <?php } ?>
-       </div> <!-- close .sl-test -->
-      </div> <!-- close .container .homepage -->
-  </div>  <!-- end .container -->
-</div> <!-- end .slider-->
+          <?php } ?> <!--closes for each loop -->
+    </div> 
+ </div> <!-- close .sl-test -->
+</div> <!-- close .container .homepage --> 
+</div> <!-- end .slider -->
 
-<div class="container info">
-<div class="row">
-<div class="col-md-12">
- <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('homepageabout') ) : ?>
-      <?php endif; ?>
-</div>
-</div>
-</div>
+<!-- BLOCK 2--PARTICIPATE -->
 
+<main class="participatehome" role="main">
+    <div class="container" id="participate">
+        <div class="row text-center">
+            <div class="col-md-3">
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate1') ) : ?>
+                <?php endif; ?>
+            </div>
+           
+            <div class="col-md-3">
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate2') ) : ?>
+                <?php endif; ?>
+            </div>
+  
+            <div class="col-md-3">
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate3') ) : ?>
+                <?php endif; ?>
+            </div>
+
+            <div class="col-md-3">
+                <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate4') ) : ?>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</main>
+
+<!-- BLOCK 3: CATEGORIES -->
 <main class="categorieshome" role="main">
 <div class="container" id="categories">
   <div class="row text-center">
-   <!--  <div class="row text-center"> -->
-    
-       <!-- </div> -->
-       <div class="row homecategories">
+    <div class="row homecategories">
     <!--Section 1-->
     <div class="col-sm-4 about" id="content1">
       <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('content1') ) : ?>
@@ -108,28 +113,15 @@ Template Name: Home Page Template
 </main>
 
 
-<main class="participatehome" role="main">
-<div class="container" id="participate">
-  <div class="row text-center">
-  <div class="col-md-3">
-  <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate1') ) : ?>
+
+  <div class="container info">
+<div class="row">
+<div class="col-md-12">
+ <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('homepageabout') ) : ?>
       <?php endif; ?>
-  </div>
-  <div class="col-md-3">
-   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate2') ) : ?>
-      <?php endif; ?>
-  </div>
-  <div class="col-md-3">
-   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate3') ) : ?>
-      <?php endif; ?>
-  </div>
-  <div class="col-md-3">
-   <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('participate4') ) : ?>
-      <?php endif; ?>
-  </div>
-  </div>
-  </div>
-  </main>
+</div>
+</div>
+</div>
 <main class="bloghome" role="main">
 <div class="container" id="blog">
 <div class="row">

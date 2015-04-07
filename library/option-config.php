b820297b988728168e189cc89c3b0af883557da2
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-04-07 11:38:59 -0400

    Adding documentation/theme setup section.

--- library/option-config.php
+++ library/option-config.php
@@ -837,7 +837,14 @@ $this->sections[] = array(
 
 
 
-
+			$this->sections[] = array(
+				'icon' => 'fa fa-info',
+				'title' => __('Getting Started', 'brew-framework'),
+				'desc' => __('<p>This theme was designed to compliment and extend the functionality of the PressForward plugin.  While the plugin is not required to use this theme, some of the functionality, options, and design of this theme was created with the plugin in mind. For more information on PressFoward visit www.pressforward.org</p>
+					<h1>Theme Setup</h1>
+					<p> Please see our GitHub wiki for a full tutorial on setting up this theme.</p>
+					')
+				);
 
 
 
@@ -900,12 +907,12 @@ $this->sections[] = array(
 	            
 	            // TYPICAL -> Change these values as you need/desire
 				'opt_name'          	=> 'brew_options', // This is where your data is stored in the database and also becomes your global variable name.
-				'display_name'			=> 'BREW Options', // $theme->get('Name'), // Name that appears at the top of your panel
+				'display_name'			=> 'PressForward TurnKey Theme Options', // $theme->get('Name'), // Name that appears at the top of your panel
 				'display_version'		=> '', //$theme->get('Version'), // Version that appears at the top of your panel
 				'menu_type'          	=> 'menu', //Specify if the admin menu should appear or not. Options: menu or submenu (Under appearance only)
 				'allow_sub_menu'     	=> true, // Show the sections below the admin menu item or not
-				'menu_title'			=> __( 'BREW Options', 'brew-framework' ),
-	            'page'		 	 		=> __( 'Brew Options', 'brew-framework' ),
+				'menu_title'			=> __( 'PressForward Theme Options', 'brew-framework' ),
+	            'page'		 	 		=> __( 'PressForward Theme Options', 'brew-framework' ),
 	            'google_api_key'   	 	=> '', // Must be defined to add google fonts to the typography module
 	            'global_variable'    	=> '', // Set a different name for your global variable other than the opt_name
 	            'dev_mode'           	=> false, // Show the time the page took to load, etc
@@ -961,7 +968,7 @@ $this->sections[] = array(
 	 
 			// Panel Intro text -> before the form
 			
-				$this->args['intro_text'] = __('<p>Welcome to the BREW options framework (powered by Redux).  Simple options are provided.  You may add more by configuring option-config.php</p>', 'brew-framework');
+				$this->args['intro_text'] = __('<p>Welcome to the PressForward TurnKey Theme options framework (powered by Redux).', 'brew-framework');
 			
 
 			// Add content after the form.

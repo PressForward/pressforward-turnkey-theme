f5391ddc127f6125915d8f8511695e7d1488dfec
regan008 <Mandy@CHNMD23396siMac.mesa.gmu.edu>
2015-10-07 09:51:58 -0400

    returns to original post navigation buttons. Closes #7.

--- single.php
+++ single.php
@@ -126,26 +126,26 @@ Features on this page:
 				<?php get_template_part( 'author-info' ); ?> 
 
 				<?php if ( is_single() ) {?>
-					<div id="single-post-nav">
+					  <div id="single-post-nav">
 					    <ul class="pager">
-							<?php $trunc_limit = 30; ?>
-						      <?php if( '' != get_previous_post() ) { ?>
-						        <li class="previous">
-						        
-						         <?php previous_post_link( '<span class="previous-page">%link</span>', __( '<i class="fa fa-caret-left"></i>', 'bones' ) . '&nbsp;' . brew_truncate_text( get_previous_post()->post_title, $trunc_limit ), TRUE ); ?>
-						          
-						        </li>
-						      <?php } // end if ?>
 
-						      <?php if( '' != get_next_post() ) { ?>
-						       <?php next_post_link( '<span class="no-previous-page-link next-page">%link</span>', '&nbsp;' . brew_truncate_text( get_next_post()->post_title, $trunc_limit ) . '&nbsp;' . __( '<i class="fa fa-caret-right"></i>', 'bones' ), TRUE ); ?>
+					      <?php $trunc_limit = 30; ?>
 
+					      <?php if( '' != get_previous_post() ) { ?>
+					        <li class="previous">
+					          <?php previous_post_link( '<span class="previous-page">%link</span>', __( '<i class="fa fa-caret-left"></i>', 'bones' ) . '&nbsp;' . brew_truncate_text( get_previous_post()->post_title, $trunc_limit ) ); ?>
+					        </li>
+					      <?php } // end if ?>
 
+					      <?php if( '' != get_next_post() ) { ?>
+					        <li class="next">
+					          <?php next_post_link( '<span class="no-previous-page-link next-page">%link</span>', '&nbsp;' . brew_truncate_text( get_next_post()->post_title, $trunc_limit ) . '&nbsp;' . __( '<i class="fa fa-caret-right"></i>', 'bones' ) ); ?>
+					        </li>
+					      <?php } // end if ?>
 
-						      <?php } // end if ?>
-						</ul>
-					</div><!-- #single-post-nav -->
-				<?php } ?>
+					    </ul>
+					  </div><!-- /#single-post-nav -->
+					<?php } ?>
 		
 		<!-- 04. COMMENT TEMPLATE -->
           <?php global $brew_options;

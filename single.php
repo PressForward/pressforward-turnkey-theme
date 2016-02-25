4b4b01ec9b681ef910460e9d6d83f7496f7d17f8
Amanda Regan <Amanda@Amandas-iMac.local>
2016-02-25 10:57:00 -0500

    added missing spaces between EC name and EL statement.

--- single.php
+++ single.php
@@ -109,13 +109,13 @@ Features on this page:
 
 							$nomcount = get_post_meta($post->ID, 'nomination_count', true);
 						if (in_category($slidercat) && $currentpostdate < '2016/02/22') {
-							echo '<div class="col-md-9 editor"><p>This content was selected for <em>Digital Humanities Now</em> by Editor-in-Chief' . $chief . ' based on nominations by Editors-at-Large: ' . $large . '</p></div>';
+							echo '<div class="col-md-9 editor"><p>This content was selected for <em>Digital Humanities Now</em> by Editor-in-Chief ' . $chief . ' based on nominations by Editors-at-Large: ' . $large . '</p></div>';
 
 						} elseif (in_category( $slidercat) && $currentpostdate > '2016/02/22') {
 							$chief = get_post_meta($post->ID, 'editor-in-chief', true);
 							$El_statement = get_post_meta($post->ID, 'editors-at-large-statement');
 
-							echo '<div class="col-md-9 editor"><p>This content was selected for <em>Digital Humanities Now</em> by Editor-in-Chief' . $chief . ' based on nominations by Editors-at-Large: ' . implode($El_statement) . '</p></div>';
+							echo '<div class="col-md-9 editor"><p>This content was selected for <em>Digital Humanities Now</em> by Editor-in-Chief ' . $chief . ' based on nominations by Editors-at-Large: ' . implode($El_statement) . '</p></div>';
 						} else {
 							echo '<span class="tags pull-left">';
 							printf( '<span class="">' . __( 'in %1$s&nbsp;&nbsp;', 'bonestheme' ) . '</span>', get_the_category_list(', ') ); ?> <?php the_tags( '<span class="tags-title">' . __( '<i class="fa fa-tags"></i>', 'bonestheme' ) . '</span> ', ', ', '' );

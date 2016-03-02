5565a97ef863660e16ace0875e8088815cfb0006
Amanda Regan <aregan2@gmu.edu>
2016-03-02 15:34:49 -0500

    adds back in the slider num posts and category options to reconfigured orbit slider

--- page-home.php
+++ page-home.php
@@ -12,7 +12,17 @@ Template Name: Home Page Template
   	<ul class="orbit-container">
     	<button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
     	<button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-			<?php $postcats = 'category=80&posts_per_page=4';
+
+			<?php
+			//get slider category ids from customizer option
+			$pftk_opts = '';
+			$slider_category = '';
+			$slider_numposts = '';
+			$slider_categories_option = Kirki::get_option( $pftk_opts, $slider_category);
+			//get number of posts from customizer option
+			$slider_numposts_option = Kirki::get_option( $pftk_opts, $slider_numposts );
+
+			$postcats = 'category='. $slider_categories_option . '&posts_per_page=' . $slider_numposts_option;
 			$feat_posts = get_posts($postcats);
 			foreach($feat_posts as $post) {
         $trim_title = get_post_field('post_title', $id);

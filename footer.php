47a47413f114901fb635046d41a54d158cec8067
Amanda Regan <aregan2@gmu.edu>
2016-04-11 11:50:01 -0400

    remove offcanvas responsive nav. Shift to top menu bar on small screen. Integrate pop up search bar next to nav bar links.

--- footer.php
+++ footer.php
@@ -30,8 +30,6 @@
 					</footer> <!-- end .footer -->
 				<?php endif; //end if $toggle_footer ?>
 				</div>  <!-- end .main-content -->
-			</div> <!-- end .off-canvas-wrapper-inner -->
-		</div> <!-- end .off-canvas-wrapper -->
 		<?php wp_footer(); ?>
 	</body>
 </html> <!-- end page -->

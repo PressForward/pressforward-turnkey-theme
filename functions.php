fbc433ffb67053492e8ff9ce4b071cda65512c0d
Amanda Regan <aregan2@gmu.edu>
2016-06-06 11:46:37 -0400

    suggest that pressforward be installed when the theme is installed.

--- functions.php
+++ functions.php
@@ -102,4 +102,36 @@ function custom_breadcrumb() {
 if ( ! isset( $content_width ) ) {
 	$content_width = 600;
 }
+
+//add tgm plugin
+require_once get_template_directory() . '/vendor/tgm-plugin-activation/class-tgm-plugin-activation.php';
+
+add_action( 'tgmpa_register', 'pftk_register_required_plugins' );
+
+function pftk_register_required_plugins() {
+	$plugins = array(
+  array(
+    'name'      => 'PressForward',
+    'slug'      => 'pressforward',
+    'required'  => false,
+  ),
+);
+$config = array(
+  'id'           => 'pressforward_tk_theme',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+  'default_path' => '',                      // Default absolute path to bundled plugins.
+  'menu'         => 'tgmpa-install-plugins', // Menu slug.
+  'has_notices'  => true,                    // Show admin notices or not.
+  'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+  'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+  'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+  'message'      => '',                       // Message to output right before the plugins table.
+);
+
+tgmpa($plugins, $config);
+}
+
+
+
+
+
 ?>
